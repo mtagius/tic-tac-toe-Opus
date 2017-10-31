2adc77b2a50b6bfc7b6c0bd134388d8593696775
Matthew Agius <mtagius@gmail.com>
2017-10-30 21:28:51 -0400

    online multiplayer code to load and join the game

--- js/onlineMultiplayer.js
+++ js/onlineMultiplayer.js
@@ -6,6 +6,49 @@ function testServer() {
 */
 
 serverName = "";
+sessionKey = "";
+spectator = false;
+
+function nameModal(player) {
+    $.get("../modals/singlePlayer.html", function(data){
+        $("#modalContainer").html(data);
+        $('#myModal').modal({
+            backdrop: 'static',
+            keyboard: false
+        })
+        $("#player1NameModal").on('input', function() {                
+            if($("#player1NameModal").val() != "") {
+                $("#beginButton").removeClass("disabled");
+            } else {
+                $("#beginButton").addClass("disabled");
+            }
+        });
+        $('#player1NameModal').keypress(function(e){
+            if(e.keyCode==13) {
+                $("#beginButton").click();
+            }
+          });
+        $("#beginButton").on('click', function() {
+            if(!($("#beginButton").hasClass("disabled"))) {
+                $('#myModal').modal("hide");
+                $("#player1NameModal").off("input");
+                $("#player1NameModal").off("keypress");
+                $("#beginButton").off("click");
+                if(player == 1) {
+                    player1Name = $("#player1NameModal").val();
+                    createCookie("playerName", player1Name);
+                    $("#player1Name").html(player1Name);
+                    //ajax to update player1Name
+                } else {
+                    player2Name = $("#player1NameModal").val();
+                    createCookie("playerName", player2Name);
+                    $("#player2Name").html(player2Name);
+                    //ajax to update player2Name
+                }
+            }
+        });
+    });
+}
 
 function initializeOnlineMultiplayer() {
     
@@ -25,15 +68,49 @@ function initializeOnlineMultiplayer() {
         console.log("Click");
     });
 
-    $("#player1Wins").html(player1Wins);
-    $("#player2Wins").html(player2Wins);
-    $("#gamesPlayed").html("Games Played: " + gamesPlayed);
-
     $.getJSON("../gameData/" + serverName + ".json", function(json) {
-        player1Name = json.players[0];
-        player2Name = json.players[1];
+
+        if(json.players[0] == "" || json.players[1] == "") {
+            if(json.players[0] == "") {
+                if(getCookie("playerName") == null) {
+                    nameModal(1);
+                } else {
+                    player1Name = getCookie("playerName");
+                }
+                player2Name =  json.players[1];
+                //ajax to update player1Name
+            }
+             else {
+                if(getCookie("playerName") == null) {
+                    nameModal(2);
+                } else {
+                    player2Name = getCookie("playerName");
+                }
+                player1Name =  json.players[0];
+                //ajax to update player2Name
+             }
+             sessionKey = json.sessionKey;
+             createCookie("sessionKey", sessionKey);
+        } else {
+            player1Name =  json.players[0];
+            player2Name =  json.players[1];
+            if(getCookie("sessionKey") != json.sessionKey) {
+                spectator = true;
+                $("#GameMode").html("Spectator");
+            }
+        }
+        
+        player1Wins = json.wins[0];
+        player2Wins = json.wins[1];
+        gamesPlayed = json.gamesPlayed;
+        turn = json.turn;
+        board = json.board;
+
         $("#player1Name").html(player1Name);
         $("#player2Name").html(player2Name);
+        $("#player1Wins").html(player1Wins);
+        $("#player2Wins").html(player2Wins);
+        $("#gamesPlayed").html("Games Played: " + gamesPlayed);
         whosTurn();
     });
 }
--- js/serverPage.js
+++ js/serverPage.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    if(getCookie("playerName") != null) {
+        $("#playerName").html(getCookie("playerName"));
+    }
     $.getJSON("../gameData/servers.json", function(json) {
         for(var i = 0; i < json.servers.length; i++) {
             $("#serverList").append('<a href="index.html?=' + json.servers[i] + '"><li>' + json.servers[i] + '</li></a>');
